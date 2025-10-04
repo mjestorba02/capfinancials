0ada89f0351f93bde53e45da9549cfc387b1d393
Estorba <03.markjohn.30@gmail.com>
2025-10-04 21:53:54 +0800

    Updated

--- api/collections.php
+++ api/collections.php
@@ -1,4 +1,11 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 0); // don't echo errors in response
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/error_log'); // log instead of output
+header('Content-Type: application/json');
+
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
